fa088ef2be985536e7da22bf998a0ac319f054c7
Marton Farago <161203082+martonfarago@users.noreply.github.com>
2026-07-01 08:43:56 +0200

    Initial Commit

---
+++ collectionViewMemoryTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collectionViewMemoryTest
+//
+//  Created by Inter Ferenc on 01/07/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
